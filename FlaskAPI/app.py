440c94b4d95cec953ce9bed8bf36ae9ee231fe73
Hassan Ademola <dewumi205@gmail.com>
2022-04-24 14:30:39 +0100

    modified

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -16,19 +16,16 @@ def home():
 
 @app.route('/predict', methods=['GET'])
 def predict():
-    try:
-        data = request.get_json()
-        keys = data.keys()
-        values = data.values()
-        df = pd.DataFrame([values],columns=keys)
-        X = df[['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors',
-            'waterfront', 'view', 'condition', 'sqft_above', 'sqft_basement',
-            'yr_built', 'yr_renovated', 'street', 'city', 'zip']]
-            
-        model = load_model()
-        y = round(float(model.predict(X)),2)
-        response = json.dumps({'price': y})
-        return response, 200
-    except:
-        return json.dumps({'response':'error'})
+    data = request.get_json()
+    keys = data.keys()
+    values = data.values()
+    df = pd.DataFrame([values],columns=keys)
+    X = df[['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors',
+        'waterfront', 'view', 'condition', 'sqft_above', 'sqft_basement',
+        'yr_built', 'yr_renovated', 'street', 'city', 'zip']]
+        
+    model = load_model()
+    y = round(float(model.predict(X)),2)
+    response = json.dumps({'price': y})
+    return response, 200
     
