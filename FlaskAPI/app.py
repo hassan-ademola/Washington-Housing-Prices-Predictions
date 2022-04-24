c9f888c1a1e77ac6a84cb28838b814c352df414b
Hassan Ademola <dewumi205@gmail.com>
2022-04-24 14:22:45 +0100

    modified

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -10,19 +10,25 @@ app.config['DEBUG'] = True
 def load_model():
     return pickle.load(open('models/model.pkl','rb'))
 
+@app.route('/', methods=['GET'])
+def home():
+    return '<h1>Washington Housing Prices</h1>'
+
 @app.route('/predict', methods=['GET'])
 def predict():
-    
-    data = request.get_json()
-    keys = data.keys()
-    values = data.values()
-    df = pd.DataFrame([values],columns=keys)
-    X = df[['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors',
-        'waterfront', 'view', 'condition', 'sqft_above', 'sqft_basement',
-        'yr_built', 'yr_renovated', 'street', 'city', 'zip']]
-        
-    model = load_model()
-    y = round(float(model.predict(X)),2)
-    response = json.dumps({'price': y})
-    return response, 200
+    try:
+        data = request.get_json()
+        keys = data.keys()
+        values = data.values()
+        df = pd.DataFrame([values],columns=keys)
+        X = df[['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors',
+            'waterfront', 'view', 'condition', 'sqft_above', 'sqft_basement',
+            'yr_built', 'yr_renovated', 'street', 'city', 'zip']]
+            
+        model = load_model()
+        y = round(float(model.predict(X)),2)
+        response = json.dumps({'price': y})
+        return response, 200
+    except:
+        return json.dumps({'response':'error'})
     
